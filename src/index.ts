371d066d213e27dbbee74d434cade6f0c28b5270
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-08-09 19:09:39 +0200

    Adding theme toggle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 import "./styles/main.scss";
+
+class Calculator {
+  themeType: string = "theme-1";
+  toggles: Element[];
+  appContainer: Element;
+  constructor() {
+    this.toggles = Array.from(document.getElementById("toggle").children);
+    this.appContainer = document.querySelector("#app");
+    this.setToggleClick();
+  }
+
+  setToggleClick() {
+    this.toggles.forEach((element, i) => {
+      element.addEventListener("click", () => {
+        this.appContainer.classList.remove(this.themeType);
+        this.appContainer.classList.add(`theme-${i + 1}`);
+        this.themeType = `theme-${i + 1}`;
+      });
+    });
+  }
+}
+
+new Calculator();
