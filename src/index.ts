410c9cbd8cd1d08b15ee01a939e588cca3ddea46
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-08-10 19:36:23 +0200

    Adding value to result fild

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,76 @@
 import "./styles/main.scss";
 
+enum ValueType {
+  first,
+  second,
+}
+
 class Calculator {
   themeType: string = "theme-1";
+  firstValue: string = "";
+  secondValue: string = "";
+  actionType: string = "";
   toggles: Element[];
   appContainer: Element;
   inputs: Element[];
+  result: Element;
   constructor() {
     this.toggles = Array.from(document.getElementById("toggle").children);
     this.appContainer = document.querySelector("#app");
     this.inputs = Array.from(document.querySelectorAll(".input"));
+    this.result = document.querySelector("#result");
     this.setToggleClick();
     this.setInputsClick();
-    console.log(this.inputs);
+  }
+
+  refreshValue(type: ValueType) {
+    if (type === ValueType.first) {
+      this.result.textContent = this.firstValue;
+    }
+
+    if (type === ValueType.second) {
+      this.result.textContent = this.secondValue;
+    }
+  }
+
+  deleteFromValue() {
+    if (this.actionType === "") {
+      if (this.firstValue.length > 1) {
+        this.firstValue = this.firstValue.slice(0, -1);
+        this.refreshValue(ValueType.first);
+      } else {
+        this.firstValue = "0";
+        this.refreshValue(ValueType.first);
+      }
+    } else {
+    }
+  }
+
+  setOperator(operatorType: string) {
+    switch (operatorType) {
+      case "DEL":
+        this.deleteFromValue();
+      default:
+    }
   }
 
   setInputsClick() {
     this.inputs.forEach((input) => {
-      input.addEventListener("click", () => {
-        console.log(input.getAttribute("data-value"));
-      });
+      if (input.getAttribute("data-type") === "number") {
+        input.addEventListener("click", () => {
+          if (this.actionType === "") {
+            this.firstValue += input.getAttribute("data-value");
+            this.refreshValue(ValueType.first);
+          } else {
+            this.secondValue += input.getAttribute("data-value");
+            this.refreshValue(ValueType.second);
+          }
+        });
+      } else {
+        input.addEventListener("click", () => {
+          this.setOperator(input.getAttribute("data-value"));
+        });
+      }
     });
   }
   setToggleClick() {
